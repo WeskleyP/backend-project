8708acc514ea8830c8cc7ca2a0f905b65b63b9cb
WeskleyP <weskleypedro@gmail.com>
2023-05-20 07:07:06 -0300

    First Commit

---
+++ src/main/java/br/com/weskley/cayena/CayenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.weskley.cayena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CayenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CayenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weskley/cayena/CayenaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weskley.cayena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CayenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
